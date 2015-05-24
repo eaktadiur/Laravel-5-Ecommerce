1c45f3b1494e5b0957c80f8b2e88889fbf00751d
hope-soft <hope.soft.bd@gmail.com>
2015-05-24 21:37:35 +0600

    init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+ @extends('app')
+ @section('content-wrapper')
+
+ <div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Dashboard
+      <small>Version 2.0</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Dashboard</li>
+    </ol>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <!-- Info boxes -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>jjjjjjjjjjjjjjjj</h1>
+      </div>
+    </div><!-- /.row -->
+  </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+@stop()
